95332eef71a5bf8599f5a426fe86cd5ab082e895
Khalid Jamal <khalidjamal11@hotmail.com>
2017-11-30 19:41:40 +0100

    Part1

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProjectExercise15
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      // task 1
+      Console.WriteLine("Indtast venligst et beløb");
+      int indtastBeløb = Convert.ToInt32(Console.ReadLine());
+      ShowPersonWSalaryHigherThan(indtastBeløb);
+      // task 2
+      MovePeopleToArrayWSalaryHigherThan(200000);
+    }
+    private static void ShowPersonWSalaryHigherThan(int salary)
+    {
+      eksempeldbEntities db = new eksempeldbEntities();
+      Console.WriteLine("Returner løninger der er højere end " + salary);
+      Console.WriteLine();
+      var t = from l in db.people
+              where l.loen > salary
+              select l;
+      foreach (var loen in t)
+      {
+        Console.WriteLine(loen.loen);
+      }
+      Console.ReadLine();
+    }
+    private static void MovePeopleToArrayWSalaryHigherThan(int salary)
+    {
+      eksempeldbEntities db = new eksempeldbEntities();
+      IEnumerable<person> arrayperson = null;
+
+
+      var t = from l in db.people
+              where l.loen > salary
+              select l;
+
+      arrayperson = t.ToArray();
+
+      foreach (var loninger in arrayperson)
+      {
+        Console.WriteLine(loninger.loen);
+      }
+      Console.ReadLine();
+    }
+    private static void ShowPeopleInACompany()
+    {
+      eksempeldbEntities db = new eksempeldbEntities();
+      var peopleincompany = from person in db.people
+                            where person.firmas.Count > 0
+                            select person;
+    }
+  }
+}
